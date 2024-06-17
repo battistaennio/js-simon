c5fddc55bc6295d79152ae929def946bc3f2893b
battistaennio <ennio.battista99@gmail.com>
2024-06-17 14:22:35 +0200

    feat: add array random number, print array in page

--- js/main.js
+++ js/main.js
@@ -3,3 +3,8 @@
 // Dopo 30 secondi i numeri scompaiono e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 
+//creo array per generare 5 numeri casuali
+const randomArray = arrayRandomNumbers(1, 50, 5)
+
+//seleziono in pagina il p dove far apparire i numeri casuali
+document.querySelector("p").innerText = randomArray;
